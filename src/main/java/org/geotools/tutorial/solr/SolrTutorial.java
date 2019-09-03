868547134e957bc5321b100074bb3723233fd32d
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-09-03 09:13:15 -0700

    Libraries are mess, but I have them installed locally.

--- src/main/java/org/geotools/tutorial/solr/SolrTutorial.java
+++ src/main/java/org/geotools/tutorial/solr/SolrTutorial.java
@@ -1,6 +1,24 @@
 package org.geotools.tutorial.solr;
 
+import static org.geotools.data.solr.SolrDataStoreFactory.FIELD;
+import static org.geotools.data.solr.SolrDataStoreFactory.URL;
+
+import java.io.IOException;
+import java.util.Map;
+import org.geotools.data.solr.SolrDataStore;
+import org.geotools.data.solr.SolrDataStoreFactory;
+import org.geotools.data.solr.SolrLayerConfiguration;
+
 public class SolrTutorial {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) throws IOException {
+
+    SolrDataStoreFactory dataStoreFactory = new SolrDataStoreFactory();
+    SolrDataStore dataStore =
+        (SolrDataStore)
+            dataStoreFactory.createDataStore(
+                Map.of(URL.key, "http://localhost:8080/solr/geotools", FIELD.key, "foo"));
+
+    Map<String, SolrLayerConfiguration> config = dataStore.getSolrConfigurations();
+  }
 }
--- src/test/java/org/geotools/tutorial/solr/SolrTutorialTest.java
+++ src/test/java/org/geotools/tutorial/solr/SolrTutorialTest.java
@@ -1,17 +1,26 @@
 package org.geotools.tutorial.solr;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.geotools.data.solr.SolrDataStoreFactory.FIELD;
+import static org.geotools.data.solr.SolrDataStoreFactory.URL;
 
+import java.io.IOException;
+import java.util.Map;
+import org.geotools.data.solr.SolrDataStore;
+import org.geotools.data.solr.SolrDataStoreFactory;
+import org.geotools.data.solr.SolrLayerConfiguration;
 import org.junit.jupiter.api.Test;
 
 public class SolrTutorialTest {
 
   @Test
-  public void testDivide() {
-    assertThrows(
-        ArithmeticException.class,
-        () -> {
-          Integer.divideUnsigned(42, 0);
-        });
+  public void test() throws IOException {
+
+    SolrDataStoreFactory dataStoreFactory = new SolrDataStoreFactory();
+    SolrDataStore dataStore =
+        (SolrDataStore)
+            dataStoreFactory.createDataStore(
+                Map.of(URL.key, "http://localhost:8080/solr/test_core", FIELD.key, "foo"));
+
+    Map<String, SolrLayerConfiguration> config = dataStore.getSolrConfigurations();
   }
 }

c93d487d7dc85a2d60a7e985d727aff567663945
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-09-03 08:33:28 -0700

    Hacked around JUnit5 issue

--- src/test/java/org/geotools/tutorial/solr/SolrTutorialTest.java
+++ src/test/java/org/geotools/tutorial/solr/SolrTutorialTest.java
@@ -1,7 +1,17 @@
 package org.geotools.tutorial.solr;
 
-class SolrTutorialTest {
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-  //  @Test
-  public void test() {}
+import org.junit.jupiter.api.Test;
+
+public class SolrTutorialTest {
+
+  @Test
+  public void testDivide() {
+    assertThrows(
+        ArithmeticException.class,
+        () -> {
+          Integer.divideUnsigned(42, 0);
+        });
+  }
 }

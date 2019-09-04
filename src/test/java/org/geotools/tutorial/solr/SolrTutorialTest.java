b1f6bfd4f68eb7bddc94fca928ec20164f17621e
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-09-03 23:09:16 -0700

    Generate docker image

--- src/test/java/org/geotools/tutorial/solr/SolrTutorialTest.java
+++ src/test/java/org/geotools/tutorial/solr/SolrTutorialTest.java
@@ -1,14 +1,18 @@
 package org.geotools.tutorial.solr;
 
-import static org.geotools.data.solr.SolrDataStoreFactory.FIELD;
+import static org.geotools.data.solr.SolrDataStoreFactory.LAYER_MAPPER;
 import static org.geotools.data.solr.SolrDataStoreFactory.URL;
 
 import java.io.IOException;
 import java.util.Map;
+import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.solr.SolrDataStore;
 import org.geotools.data.solr.SolrDataStoreFactory;
-import org.geotools.data.solr.SolrLayerConfiguration;
+import org.geotools.data.solr.SolrFeatureSource;
+import org.geotools.data.solr.SolrLayerMapper.Type;
 import org.junit.jupiter.api.Test;
+import org.opengis.filter.FilterFactory;
+import org.opengis.filter.spatial.BBOX;
 
 public class SolrTutorialTest {
 
@@ -16,11 +20,25 @@ public class SolrTutorialTest {
   public void test() throws IOException {
 
     SolrDataStoreFactory dataStoreFactory = new SolrDataStoreFactory();
+    String layerName = Type.SINGLE.name();
     SolrDataStore dataStore =
         (SolrDataStore)
             dataStoreFactory.createDataStore(
-                Map.of(URL.key, "http://localhost:8080/solr/test_core", FIELD.key, "foo"));
+                Map.of(
+                    URL.key, "http://localhost:8080/solr/test_core", LAYER_MAPPER.key, layerName));
 
-    Map<String, SolrLayerConfiguration> config = dataStore.getSolrConfigurations();
+    // What is this? FeatureType names? ... No it is the name of the Solr cores.
+    String[] typeNames = dataStore.getTypeNames();
+
+    // Get source
+    SolrFeatureSource featureSource = (SolrFeatureSource) dataStore.getFeatureSource(layerName);
+
+    // Construct filter
+    int SOURCE_SRID = 4326;
+    FilterFactory ff = dataStore.getFilterFactory();
+    BBOX bbox = ff.bbox("geo", -185, -98, 185, 98, "EPSG:" + SOURCE_SRID);
+
+    // Get results fom Solr
+    SimpleFeatureCollection features = featureSource.getFeatures(bbox);
   }
 }
